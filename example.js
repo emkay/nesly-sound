cb291c6f594992a53f6a3ccf061892d385560578
Michael Matuzak <mmatuzak@gmail.com>
2015-04-06 14:19:45 -0700

    translate notes array to square byte

---
+++ example.js
@@ -0,0 +1,11 @@
+var sound = require('./');
+var squareMusic = [
+    'B2', 'D3', 'F3', 'Gs3',
+    'B3', 'D4', 'F4', 'Gs4',
+    'B4', 'D5', 'F5', 'Gs5',
+    'B5', 'D6', 'F6', 'Gs6'
+];
+
+sound.square(squareMusic);
+
+sound.compile();
--- index.js
+++ index.js
@@ -3,4 +3,16 @@ var bootstrap = fs.readFileSync('./bootstrap.s');
 
 var out = bootstrap;
 
-console.log(out.toString());
+function square(notes) {
+    var s = notes.join(',');
+    out += '\t.byte: ' + s;
+}
+
+function compile() {
+    console.log(out.toString());
+}
+
+module.exports = {
+    square: square,
+    compile: compile
+};
