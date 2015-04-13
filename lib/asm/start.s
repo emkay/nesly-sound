59ebdc270ebe34a350f5ab884ea3f1a8f56f22e0
Michael Matuzak <mmatuzak@gmail.com>
2015-04-13 12:35:44 -0700

    new asm sound engine

--- lib/asm/start.s
+++ lib/asm/start.s
@@ -7,28 +7,29 @@
 joypad1 .rs 1           ;button states for the current frame
 joypad1_old .rs 1       ;last frame's button states
 joypad1_pressed .rs 1   ;current frame's off_to_on transitions
-sleeping .rs 1          ;main program sets this and waits for the NMI to clear it.  Ensures the main program is run only once per frame.  
+sleeping .rs 1          ;main program sets this and waits for the NMI to clear it.  Ensures the main program is run only once per frame.
                         ;   for more information, see Disch's document: http://nesdevhandbook.googlepages.com/theframe.html
 needdraw .rs 1          ;drawing flag.
 dbuffer_index .rs 1     ;current position in the drawing buffer
 ptr1 .rs 2              ;a pointer
 sound_ptr .rs 2
+sound_ptr2 .rs 2
 current_song .rs 1
-    
+
 ;----- first 8k bank of PRG-ROM
     .bank 0
     .org $8000  ;we have two 16k PRG banks now.  We will stick our sound engine in the first one, which starts at $8000.
-    
+
     .include "sound_engine.s"
 
-;----- second 8k bank of PRG-ROM    
+;----- second 8k bank of PRG-ROM
     .bank 1
     .org $A000
-    
-;----- third 8k bank of PRG-ROM    
+
+;----- third 8k bank of PRG-ROM
     .bank 2
     .org $C000
-    
+
 irq:
     rti
 NMI:
@@ -37,29 +38,29 @@ NMI:
     pha
     tya
     pha
-    
+
     ;do sprite DMA
     ;update palettes if needed
     ;draw stuff on the screen
-    
+
     lda needdraw
     beq .drawing_done   ;if drawing flag is clear, skip drawing
     lda $2002           ;else, draw
     jsr draw_dbuffer
     lda #$00            ;finished drawing, so clear drawing flag
     sta needdraw
-    
-.drawing_done:    
+
+.drawing_done:
     lda #$00
     sta $2005
     sta $2005   ;set scroll
-    
+
     jsr sound_play_frame    ;run our sound engine after all drawing code is done.
                             ;this ensures our sound engine gets run once per frame.
-                            
+
     lda #$00
     sta sleeping            ;wake up the main program
-    
+
     pla     ;restore registers
     tay
     pla
@@ -73,11 +74,11 @@ RESET:
     ldx #$FF
     txs
     inx
-    
+
 vblankwait1:
     bit $2002
     bpl vblankwait1
-    
+
 clearmem:
     lda #$00
     sta $0000, x
@@ -91,7 +92,7 @@ clearmem:
     sta $0200, x
     inx
     bne clearmem
-    
+
  vblankwait2:
     bit $2002
     bpl vblankwait2
@@ -103,17 +104,17 @@ clearmem:
     sta $2006
     lda #$00
     sta $2006   ;palette data starts at $3F00
-    
+
     lda #$0F    ;black
     sta $2007
     lda #$30    ;white
     sta $2007
-    
+
     jsr draw_background
-    
+
 ;Enable sound channels
     jsr sound_init
-    
+
     lda #$01
     sta current_song
     ;jsr sound_load
@@ -128,39 +129,39 @@ forever:
 .loop:
     lda sleeping
     bne .loop ;wait for NMI to clear the sleeping flag and wake us up
-    
+
     ;when NMI wakes us up, handle input, fill the drawing buffer and go back to sleep
     jsr read_joypad
     jsr handle_input
     jsr prepare_dbuffer
     jmp forever ;go back to sleep
-    
-    
+
+
 ;----------------------------
-; read_joypad will capture the current button state and store it in joypad1.  
+; read_joypad will capture the current button state and store it in joypad1.
 ;       Off-to-on transitions will be stored in joypad1_pressed
 read_joypad:
     lda joypad1
     sta joypad1_old ;save last frame's joypad button states
-    
+
     lda #$01
     sta $4016
     lda #$00
     sta $4016
-    
+
     ldx #$08
-.loop:    
+.loop:
     lda $4016
     lsr a
     rol joypad1  ;A, B, select, start, up, down, left, right
     dex
     bne .loop
-    
+
     lda joypad1_old ;what was pressed last frame.  EOR to flip all the bits to find ...
     eor #$FF    ;what was not pressed last frame
     and joypad1 ;what is pressed this frame
     sta joypad1_pressed ;stores off-to-on transitions
-    
+
     rts
 
 ;---------------------
@@ -193,7 +194,7 @@ handle_input:
     lda joypad1_pressed
     and #$01 ;right
     beq .done
-    jsr song_up    
+    jsr song_up
 .done:
     rts
 
@@ -207,9 +208,9 @@ song_down:
     sta current_song
 .done:
     rts
-    
+
 ;----------------------
-; song_up will move selection up a song.  Last song will wrap to song 1   
+; song_up will move selection up a song.  Last song will wrap to song 1
 song_up:
     inc current_song
     lda current_song
@@ -219,10 +220,10 @@ song_up:
     sta current_song
 .done:
     rts
-    
+
 ;-------------------------------
-; prepare_dbuffer fills the drawing buffer with the text strings we need 
-prepare_dbuffer:    
+; prepare_dbuffer fills the drawing buffer with the text strings we need
+prepare_dbuffer:
     ;write either "playing" or "not playing" to the dbuffer
     lda stream_status
     ora stream_status+1
@@ -230,6 +231,7 @@ prepare_dbuffer:
     ora stream_status+3
     ora stream_status+4
     ora stream_status+5
+    and #$01
     beq .sound_not_playing  ;if all streams disabled, write "NOT PLAYING" on the screen
     lda sound_disable_flag
     bne .sound_not_playing  ;if the disable flag is set, we want to write "NOT PLAYING" too
@@ -248,12 +250,12 @@ prepare_dbuffer:
     lda #$21    ;target PPU address.  add_to_dbuffer expects the HI byte in A and the LO byte in Y
     ldy #$0B
     jsr add_to_dbuffer
-    
+
     jsr song_num_to_dbuffer
-    
+
     lda #$01
     sta needdraw    ;set drawing flag so the NMI knows to draw
-    
+
     rts
 
 ;-------------------------
@@ -288,12 +290,12 @@ add_to_dbuffer:
     ldx dbuffer_index
     tya
     sta $0100, x        ;store string length at the beginning of the string header
-    
+
     clc
     adc dbuffer_index
-    adc #$03        
+    adc #$03
     sta dbuffer_index   ;update buffer index.  new index = old index + 3-byte header + string length
-    
+
     tax
     lda #$00
     sta $0100, x        ;stick a 0 on the end to terminate dbuffer.
@@ -318,7 +320,7 @@ song_num_to_dbuffer:
     adc #$04
     sta dbuffer_index
     rts
-    
+
 ;------------------------
 ; draw_dbuffer will write the contents of the drawing buffer to the PPU
 ;       dbuffer is made up of a series of drawing strings.  dbuffer is 0-terminated.
@@ -342,7 +344,7 @@ draw_dbuffer:
     iny
     dex
     bne .copy_loop
-    beq .header_loop    ;when we finish copying, see if there is another drawing string.    
+    beq .header_loop    ;when we finish copying, see if there is another drawing string.
 .done:
     ldy #$00
     sty dbuffer_index   ;reset index and "empty" the dbuffer by sticking a zero in the first position
@@ -379,8 +381,8 @@ draw_background:
     bne .loop2
 .done:
     rts
-    
-;----- fourth 8k bank of PRG-ROM    
+
+;----- fourth 8k bank of PRG-ROM
     .bank 3
     .org $E000
 
@@ -388,7 +390,7 @@ draw_background:
 
 text_song:
     .byte $22, $1E, $1D, $16, $0D, $FF ;"SONG:"
-    
+
 text_sound:
     .byte $22, $1E, $24, $1D, $13, $0D, $FF ;"SOUND:"
 text_not_playing:
@@ -396,7 +398,7 @@ text_not_playing:
 text_playing:
     .byte $1F, $1B, $10, $28, $18, $1D, $16, $00, $00, $00, $00, $FF ;"PLAYING    "
 
-    
+
 ;---- vectors
     .org $FFFA     ;first of the three vectors starts here
     .dw NMI        ;when an NMI happens (once per frame if enabled) the 
@@ -404,7 +406,7 @@ text_playing:
     .dw RESET      ;when the processor first turns on or is reset, it will jump
                    ;to the label RESET:
     .dw irq        ;external interrupt IRQ is not used in this tutorial
-    
+
     .bank 4
     .org $0000
-    .incbin "headers.chr"
+    .incbin "sound.chr"
