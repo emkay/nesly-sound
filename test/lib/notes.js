e6d41180bda394c8ac76d287c54c9c10b192cc5c
Michael Matuzak <mmatuzak@gmail.com>
2015-04-20 10:40:12 -0700

    updating notes test

--- test/lib/notes.js
+++ test/lib/notes.js
@@ -6,10 +6,10 @@ test('initial values', function (t) {
 
     var notes = Notes('song0_');
     t.ok(notes, 'notes is setup');
-    t.equal(notes.square1Code, 'song0_square1:\n', 'Square 1 code is incorrect');
-    t.equal(notes.square2Code, 'song0_square2:\n', 'Square 2 code is incorrect');
-    t.equal(notes.triangleCode, 'song0_tri:\n', 'Triangle code is incorrect');
-    t.equal(notes.noiseCode, 'song0_noise:\n', 'Noise code is incorrect');
+    t.equal(notes.code.square1, 'song0_square1:\n', 'Square 1 code is incorrect');
+    t.equal(notes.code.square2, 'song0_square2:\n', 'Square 2 code is incorrect');
+    t.equal(notes.code.triangle, 'song0_tri:\n', 'Triangle code is incorrect');
+    t.equal(notes.code.noise, 'song0_noise:\n', 'Noise code is incorrect');
 });
 
 test('temp code check', function (t) {
