3ec14057f361b81a5232bb8d7c1a94c192129f6c
Michael Matuzak <mmatuzak@gmail.com>
2015-05-09 15:07:36 -0700

    shifting the note table an octave down

--- examples/song.js
+++ examples/song.js
@@ -13,23 +13,26 @@ var C3 = note('C3');
 var E3 = note('E3');
 var A4 = A3.perfectOctave();
 
-var A7 = note('A7');
-var C7 = note('C7');
-var E7 = note('E7');
-//var A8 = A7.perfectOctave();
+var A6 = note('A6');
+var C6 = note('C6');
+var E6 = note('E6');
+var A7 = A6.perfectOctave();
+
+var G3 = note('G3');
+var C4 = C3.perfectOctave();
 
 square1([A3, C3, E3, A4])
     .duty(50)
     .blip()
     .loop(4);
 
-square1([A7, C7, E7, 'A8'])
+square1([A6, C6, E6, A7])
     .timing(1/4);
 
-square2([C3, E3, 'G3', 'C4'])
+square2([C3, E3, G3, C4])
     .loop(2);
 
-triangle(['C3', 'E3', 'G3', 'C4'])
+triangle([C3, E3, G3, C4])
     .loop(2);
 
 noise(['$04', '$06', '$04', '$06'])
